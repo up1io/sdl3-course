568fe43470b615a9bf974f727e6ab5a3be54fb7d
Jonathan Mausbach <mausbach@up1.io>
2025-02-04 21:43:02 +0000

    added missing sdl callback functions

--- 007-audio/main.c
+++ 007-audio/main.c
@@ -86,4 +86,15 @@ SDL_AppEvent(void *appstate, SDL_Event *event)
 
     return SDL_APP_CONTINUE;
 }
+SDL_AppResult
+SDL_AppIterate(void *appstate)
+{
+    return SDL_APP_CONTINUE;
+}
+
+void
+SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+}
+
 
